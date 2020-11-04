29c4dc10be6f01b405e08780c4958e6265705cdb
Callum Clegg <61652738+implements-callum@users.noreply.github.com>
2020-11-04 23:12:09 +0000

    Add files via upload

---
+++ Year_2/C_LABS/inputs_and_outputs.h
@@ -0,0 +1,8 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+#ifndef LABS_INPUTS_AND_OUTPUTS_H
+#define LABS_INPUTS_AND_OUTPUTS_H
+void palExtract(FILE *input, char mode); // Checks if string is a palindrome and writes accordingly.
+bool isword(const char *str); // Ensures str is a word
+#endif //LABS_INPUTS_AND_OUTPUTS_H
